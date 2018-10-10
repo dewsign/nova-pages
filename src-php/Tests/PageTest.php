d71cbe578ff5adbfd3d4d6435215fd2721e500f9
Marco Mark <marco@silvanite.com>
2018-10-10 14:51:21 +0100

    Basic tests

--- src-php/Tests/PageTest.php
+++ src-php/Tests/PageTest.php
@@ -9,9 +9,21 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 abstract class PageTest extends TestCase
 {
-    public function testRandomPageIsAccessible()
+    public function testRandomRootPageIsAccessible()
     {
-        $result = $this->get(route('pages.show', [Page::active()->inRandomOrder()->first()]));
+        $result = $this->get(route('pages.show', [
+            Page::active()->doesntHave('parent')->inRandomOrder()->first()->full_path,
+        ]));
+
+        $result->assertOk();
+    }
+
+    public function testRandomChildPageIsAccessible()
+    {
+        $result = $this->get(route('pages.show', [
+            Page::active()->has('parent')->inRandomOrder()->first()->full_path,
+        ]));
+
         $result->assertOk();
     }
 }
