ba06c57c56a622502e20415f02fb0c5da176cc17
Daniel Crewdson <daniel.crewdson@dewsign.co.uk>
2018-10-18 16:53:39 +0100

    add the preferred sizing as default

--- src-php/Config/novapages.php
+++ src-php/Config/novapages.php
@@ -4,6 +4,6 @@ return [
     'repeaters' => [],
     'replaceRepeaters' => false,
     'homepageSlug' => 'homepage',
-    'largeImageWidth' => 1600,
-    'largeImageHeight' => 900,
+    'largeImageWidth' => 1400,
+    'largeImageHeight' => 540,
 ];
