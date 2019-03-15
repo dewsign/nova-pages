c01563184d18d85bba9e6b339564976bfa4a86aa
Marco Mark <m2de@outlook.com>
2019-03-15 10:02:28 +0000

    Make homepage only accessible via the root url to fix #18

---
+++ src-php/Http/Middleware/RedirectHomepageSlugToRoot.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Dewsign\NovaPages\Http\Middleware;
+
+class RedirectHomepageSlugToRoot
+{
+    /**
+     * Ensure the homepage can not be accessed at multiple URLs
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return \Illuminate\Http\Response
+     */
+    public function handle($request, $next)
+    {
+        $homepageSlug = config('novapages.homepageSlug');
+
+        if ($homepageSlug && $request->is($homepageSlug)) {
+            return redirect('/', 301);
+        }
+
+        return $next($request);
+    }
+}
