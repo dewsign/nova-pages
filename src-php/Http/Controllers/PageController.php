fdca7852bec774ebc134f4b483c93362e5315b84
Marco Mark <m2de@outlook.com>
2019-05-09 16:41:16 +0100

    All the routes are belong to us

--- src-php/Http/Controllers/PageController.php
+++ src-php/Http/Controllers/PageController.php
@@ -4,7 +4,9 @@ namespace Dewsign\NovaPages\Http\Controllers;
 
 use Dewsign\NovaPages\Models\Page;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
@@ -13,14 +15,41 @@ class PageController extends Controller
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
+    /**
+     * The main show route to display pages. This includes domain mapped pages and the homepage.
+     * In general, this method could do with some cleaning up but everything works for now.
+     *
+     * If a domain mapped page is requested on a different domain it will automatically
+     * be redirected to the correct domain name mapped path.
+     *
+     * @param string $domain
+     * @param string $path
+     * @return \Illuminate\View\View
+     */
     public function show(string $domain = null, string $path = null)
     {
         if (!$path) {
             $path = $domain;
+            $domain = null;
         }
 
+        if ($domain && in_array($domain, config('novapages.domainMap'))) {
+            $path = str_replace(config('novapages.homepageSlug'), '', $path);
+            /**
+             * Since we are now mapping to domain names we actually need to reverse
+             * the map in order to lookup the correct page with the full path.
+             */
+            $reverseMappedPath = collect([$domain, $path])->filter()->implode('/');
+        }
+
+        if ($desiredRoute = config('novapages.models.page', Page::class)::isWithinDomainMap($domain, $path)) {
+            if ($desiredRoute !== URL::current()) {
+                return Redirect::away($desiredRoute);
+            }
+        };
+
         $page = app(config('novapages.models.page', Page::class))::withParent()
-            ->whereFullPath($path)
+            ->whereFullPath($reverseMappedPath ?? $path, $excludeMappedDomains = false)
             ->first();
 
         if (!$page) {
