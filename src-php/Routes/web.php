85a5669406e119f821e109ab196b8f39e8dce978
Marco Mark <m2de@outlook.com>
2019-05-09 16:40:33 +0100

    Allow routing to domain or non-domain routes

--- src-php/Routes/web.php
+++ src-php/Routes/web.php
@@ -3,7 +3,7 @@
 /**
  * Page Routes. Should be the last route as it doesn't have a root namespace and could take-over other routes
  */
-Route::domain('{domain}.' . config('novapages.rootDomain'))->name('pages.')->group(function () {
+Route::domain('{domain}.' . config('novapages.rootDomain'))->name('domain.')->group(function () {
     include('pages.php');
 });
 
